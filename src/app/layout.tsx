c8b8be4fba19b2920c6cd3ab6b81082e34c1a244
ndoelger <doelgern@gmail.com>
2023-12-05 21:07:32 -0500

    Finalized nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {" "}
+        {/* <Nav /> */}
         <Nav2 />
         {children}
       </body>
--- src/components/global/Nav2.tsx
+++ src/components/global/Nav2.tsx
@@ -3,7 +3,27 @@ import Image from "next/image";
 import { navElement } from "@/types/types";
 import { navsProp } from "@/types/types";
 
-const navs:navElement[] = [
+const navs: navElement[] = [
+  {
+    title: "About Us",
+    subheaders: [
+      "History & Mission",
+      "Staff",
+      "Board of Directors",
+      "Annual Reports",
+      "Financials",
+    ],
+  },
+  {
+    title: "Contact",
+    subheaders: [
+      "History & Mission",
+      "Staff",
+      "Board of Directors",
+      "Annual Reports",
+      "Financials",
+    ],
+  },
   {
     title: "About Us",
     subheaders: [
@@ -26,7 +46,7 @@ const navs:navElement[] = [
   },
 ];
 
-const Navigation = ({ navs }:navsProp ) => {
+const NavigationMobile = ({ navs }: navsProp) => {
   return (
     <>
       {navs.map(({ title, subheaders }, index) => (
@@ -45,6 +65,27 @@ const Navigation = ({ navs }:navsProp ) => {
   );
 };
 
+const NavigationDesktop = ({ navs }: navsProp) => {
+  return (
+    <>
+      {navs.map(({ title, subheaders }, index) => (
+        <li key={index}>
+          <details>
+            <summary>{title}</summary>
+            <ul className="p-2">
+              {subheaders.map((subheader, subIndex) => (
+                <li key={subIndex}>
+                  <a>{subheader}</a>
+                </li>
+              ))}
+            </ul>
+          </details>
+        </li>
+      ))}
+    </>
+  );
+};
+
 // about us
 // our impact
 // get involved
@@ -52,13 +93,22 @@ const Navigation = ({ navs }:navsProp ) => {
 
 function Nav2() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 justify-between">
       <Image
         src="./logo.svg"
         width={180}
         height={20}
         alt="Bottomless Logo"></Image>
-      <div className="navbar-start">
+      {/* <div className="navbar-start"> */}
+      {/* </div> */}
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <NavigationDesktop navs={navs} />
+        </ul>
+      </div>
+      <div>
+        <a className="btn ml-5 mr-3 hidden md:flex">GET JOB HELP</a>
+        <a className="btn mr-3 hidden md:flex">DONATE</a>
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg
@@ -77,47 +127,12 @@ function Nav2() {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li>
-              <a>About Us</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 absolute right-1">
+            <NavigationMobile navs={navs} />
+            <a className="btn mt-5">GET JOB HELP</a>
+            <a className="btn mt-3">DONATE</a>
           </ul>
         </div>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
-        <a className="btn">Button</a>
       </div>
     </div>
   );

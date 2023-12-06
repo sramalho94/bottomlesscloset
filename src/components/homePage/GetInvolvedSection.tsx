5a4f0564eb816d1d249012a5ebf7b4581f19195b
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-12-06 00:03:18 -0500

    add image of lady in get involved section

--- src/components/homePage/GetInvolvedSection.tsx
+++ src/components/homePage/GetInvolvedSection.tsx
@@ -1,29 +1,36 @@
 import React from 'react';
 
 import GetInvolvedBtn from './btns/GetInvolvedBtn';
+import LadyGetInvolved from './LadyGetInvolved';
 
 function GetInvolvedSection() {
     return (
-        // <div className="h-[368px] bg-red-600 justify-end ml-[5%] lg:ml-[10%]">
-        <div className="h-[368px] ml-[5%] mt-[24px] lg:ml-[10%] lg:mt-[58px]">
-            <div>
-                <div className='w-[40%]'>
-                    <p className='text-[20px] lg:text-[24px] text-black'>
-                        “I feel stuck sometimes, but coming to Bottomless Closet reminds
-                        me of how far I’ve come and the next step I can take. Coming here
-                        makes me feel motivated and hopeful.”
-                    </p>
+        <div className="flex flex-row h-[368px] ml-[5%] mt-[24px] lg:ml-[10%] lg:mt-[58px]">
+            {/* <div className="h-[368px] ml-[5%] mt-[24px] lg:ml-[10%] lg:mt-[58px]"> */}
+            <div className="w-[40%]">
+                <div>
+                    <div>
+                        <p className='text-[20px] lg:text-[24px] text-black'>
+                            “I feel stuck sometimes, but coming to Bottomless Closet reminds
+                            me of how far I’ve come and the next step I can take. Coming here
+                            makes me feel motivated and hopeful.”
+                        </p>
+                    </div>
+                    <div className='flex justify-end'>
+                        <p className='text-black mr-[7%] text-[20px] lg:text-[24px]'>
+                            -
+                            {/* TODO: Change text color to hex code when you have one */}
+                            <span className='text-[#B0121A]'> Diana</span>
+                        </p>
+                    </div>
                 </div>
-                <div className='flex w-[40%] justify-end'>
-                    <p className='text-black mr-[7%] text-[20px] lg:text-[24px]'>
-                        - 
-                        {/* TODO: Change text color to hex code when you have one */}
-                        <span className='text-[#B0121A]'> Diana</span>
-                    </p>
-                </div>
+
+                <GetInvolvedBtn />
+
+
             </div>
 
-            <GetInvolvedBtn />
+            <LadyGetInvolved />
 
         </div>
     )

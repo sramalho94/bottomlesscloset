f0550add8a7349b9112f34ac2344b3c798e76860
krsnamara <martinj.fitzpatrick@gmail.com>
2023-12-06 23:07:48 -0500

    Add EventCard and Hero to workshops directory in comp. Import hero png and jpeg. Add exported components to Workshop main component

--- src/pages/Workshops.tsx
+++ src/pages/Workshops.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Hero from '@/components/workshops/Hero'
+import EventCard from '@/components/workshops/EventCard'
 
 function WorkshopsPage() {
-  return <div>Workshops Pages</div>
+  return (
+    <>
+      <Hero />
+      <EventCard />
+    </>
+  )
 }
 
 export default WorkshopsPage

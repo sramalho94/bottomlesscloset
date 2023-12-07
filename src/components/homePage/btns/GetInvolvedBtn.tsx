96e8e9a29e26e0954aee9e9720de9c5ea854ec1d
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-12-07 11:21:33 -0500

    make HomeBackgroundImage text responsive

--- src/components/homePage/btns/GetInvolvedBtn.tsx
+++ src/components/homePage/btns/GetInvolvedBtn.tsx
@@ -7,8 +7,8 @@ const montserrat = Montserrat({ subsets: ['latin'] })
 function GetInvolvedBtn() {
     return (
         <Link href="/">
-            <div className="flex justify-center items-center bg-[#FFDAD6] w-[274px] h-[60px] rounded-[20px]">
-                <h1 className={`${montserrat.className} text-[24px] font-bold text-[#630A0E]`}>
+            <div className="flex justify-center items-center bg-[#FFDAD6] w-[200px] h-[40px] sm:w-[274px] sm:h-[60px] rounded-[20px]">
+                <h1 className={`${montserrat.className} text-[18px] sm:text-[24px] font-bold text-[#630A0E]`}>
                     GET INVOLVED
                 </h1>
             </div>

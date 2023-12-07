f05a01a36cd17d7bbc498a793acddabeaa866ec2
krsnamara <martinj.fitzpatrick@gmail.com>
2023-12-07 00:06:04 -0500

    Style and size text headers over hero image. Make breakpoint responsiveness.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,29 @@
 import type { Config } from 'tailwindcss'
-import daisyui from "daisyui"
-
-
-
+import daisyui from 'daisyui'
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
         'core-red': '#B0121A',
         'dark-red': '#630A0E',
         'pink-2': '#FFDAD6',
-        "salmon": '#FFB4AC',
+        salmon: '#FFB4AC',
         'dark-green': '#085047',
         'red-brick': '#8C0E14',
-        "charcoal": '#404040',
-        'light-gray': '#404040'
-      }
+        charcoal: '#404040',
+        'light-gray': '#404040',
+      },
     },
   },
   plugins: [daisyui],

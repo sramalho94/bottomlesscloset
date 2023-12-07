0e8a0e743f110cf4098f4b928db78fa8cd8371d5
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-12-06 23:00:43 -0500

    add CreditCardStep to donation modal

--- src/components/homePage/header/donationModal/CreditCardStep.tsx
+++ src/components/homePage/header/donationModal/CreditCardStep.tsx
@@ -4,16 +4,84 @@ import { PropsForDonateStep } from '@/types/types'
 
 import SubmitDonationBtn from '../../btns/SubmitDonationBtn';
 
-function CreditCardStep({setDonationStep}: PropsForDonateStep) {
+function CreditCardStep({ setDonationStep }: PropsForDonateStep) {
+
+    const [cardNumber, setCardNumber] = useState('')
+    const [expirationDate, setExpirationDate] = useState('')
+    const [CVV, setCVV] = useState('')
+    const [zipCode, setZipCode] = useState('')
+
     const handleSubmit = () => {
         console.log('Submitted ')
         setDonationStep(5)
     }
-  return (
-    <div>
-        <SubmitDonationBtn handleSubmit={handleSubmit} />
-    </div>
-  )
+    return (
+        <div className="flex flex-col justify-between h-[100%] w-[368px]">
+
+            <div className='mt-[20px]'>
+
+                <div>
+                    <p className="text-[#404040] mb-[5px]">Credit card number:</p>
+                    <div className='flex items-center p-[5px] w-[100%] h-[40px] border border-[#8692A6] rounded-[6px]'>
+                        <input
+                            className='text-[#404040] text-[16px] w-[100%] bg-transparent focus:outline-none'
+                            type='text'
+                            value={cardNumber}
+                            placeholder='1234 5678 9012 3456'
+                            onChange={(e) => setCardNumber(e.target.value)}
+                        />
+                    </div>
+                </div>
+
+                <div className="flex justify-between mt-[20px]">
+                    <div className="w-[45%]">
+                        <p className="text-[#404040] mb-[5px]">Expiration date:</p>
+                        <div className='flex items-center p-[5px] w-[100%] h-[40px] border border-[#8692A6] rounded-[6px]'>
+                            <input
+                                className='text-[#404040] text-[16px] w-[100%] bg-transparent focus:outline-none'
+                                type='text'
+                                value={expirationDate}
+                                placeholder='01/29'
+                                onChange={(e) => setExpirationDate(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div className="w-[45%]">
+                        <p className="text-[#404040] mb-[5px]">CVV:</p>
+                        <div className='flex items-center p-[5px] w-[100%] h-[40px] border border-[#8692A6] rounded-[6px]'>
+                            <input
+                                className='text-[#404040] text-[16px] w-[100%] bg-transparent focus:outline-none'
+                                type='text'
+                                value={CVV}
+                                placeholder='123'
+                                onChange={(e) => setCVV(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex justify-between mt-[20px]">
+                    <div className="w-[45%]">
+                        <p className="text-[#404040] mb-[5px]">Zip code:</p>
+                        <div className='flex items-center p-[5px] w-[100%] h-[40px] border border-[#8692A6] rounded-[6px]'>
+                            <input
+                                className='text-[#404040] text-[16px] w-[100%] bg-transparent focus:outline-none'
+                                type='text'
+                                value={zipCode}
+                                placeholder='10990'
+                                onChange={(e) => setZipCode(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+            <SubmitDonationBtn handleSubmit={handleSubmit} />
+
+        </div>
+    )
 }
 
-export default CreditCardStep
+export default CreditCardStep;

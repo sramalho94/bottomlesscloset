8087bd9d1f4c94a89e8731b6d9915af5b07caf43
noordibou <noor.dibou@hotmail.com>
2023-12-07 11:35:12 -0600

    added tailwind fonts & adjusted footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bottom-0 flex flex-col py-10 gap-11 w-full text-[#FEFAFA] bg-[#B0121A]">
+    <footer className="footer pt-6 pb-3 text-[#FEFAFA] bg-[#B0121A]">
       <div className="container mx-auto py-2">
         <div className="grid grid-cols-3 gap-8 text-xs">
           <div className="flex flex-col gap-2">
@@ -17,27 +17,27 @@ const Footer: React.FC = () => {
                 className="pb-6"
               />
             </Link>
-            <div className="flex flex-row text-[16px]">
+            <div className="flex flex-row text-sm md:text-md gap-2">
               <Image
                 src="/footer/phone.png"
-                width={20}
-                height={18}
+                width={24}
+                height={12}
                 alt="phone"
-                className="pr-2"
+                className="h-4 w-4 mt-0.5"
               />
               <span>1-212-563-2499</span>
             </div>
-            <div className="flex flex-row gap-3.5">
+            <div className="flex flex-row ">
               <Link
                 href="https://www.youtube.com/user/BottomlessCloset"
                 target="_blank"
               >
                 <Image
                   src="/footer/youtube.png"
-                  width={42}
+                  width={39}
                   height={8}
                   alt="youtube"
-                  className=""
+                  className="w-4 h-4 md: md:w-auto md:h-auto"
                 />
               </Link>
               <Link
@@ -49,7 +49,7 @@ const Footer: React.FC = () => {
                   width={39}
                   height={8}
                   alt="linkedIn"
-                  className=""
+                  className="w-4 h-4 md: md:w-auto md:h-auto"
                 />
               </Link>
               <Link
@@ -61,7 +61,7 @@ const Footer: React.FC = () => {
                   width={32}
                   height={8}
                   alt="pinterest"
-                  className=""
+                  className="w-4 h-4 md: md:w-auto md:h-auto"
                 />
               </Link>
               <Link href="https://twitter.com/BttmlessClstNYC" target="_blank">
@@ -70,7 +70,7 @@ const Footer: React.FC = () => {
                   width={29}
                   height={8}
                   alt="twitter"
-                  className=""
+                  className="w-4 h-4 md: md:w-auto md:h-auto"
                 />
               </Link>
               <Link
@@ -82,82 +82,82 @@ const Footer: React.FC = () => {
                   width={32}
                   height={8}
                   alt="facebook"
-                  className=""
+                  className="w-4 h-4 md: md:w-auto md:h-auto"
                 />
               </Link>
             </div>
           </div>
-          <div className="flex flex-col text-neutral-50 text-xl font-normal font-['Open Sans'] underline gap-2.5 leading-7">
-            <span>Donate Now</span>
-            <span>Contact Us</span>
-            <span>Get Career Help</span>
-            <span>Careers</span>
-            <span>Workshops</span>
-            <span>Get Involved</span>
-            <span>News & Events</span>
+          <div className="flex flex-col text-neutral-50 text-sm md:text-md font-normal font-osans gap-1">
+            <Link href={""} className="link link-hover">Donate Now</Link>
+            <Link href={""} className="link link-hover">Contact Us</Link>
+            <Link href={""} className="link link-hover">Get Career Help</Link>
+            <Link href={""} className="link link-hover">Careers</Link>
+            <Link href={""} className="link link-hover">Workshops</Link>
+            <Link href={""} className="link link-hover">Get Involved</Link>
+            <Link href={""} className="link link-hover">News & Events</Link>
           </div>
-          <div className="flex flex-col gap-2">
-            <div className="">
-              <span className="text-neutral-50 text-[28px] font-bold font-['Nunito Sans'] leading-[33.60px]">
+          <div className="flex flex-col">
+            <div className="mt-6">
+              <span className="text-neutral-50 text-md md:text-lg font-bold font-mont ">
                 Stay Informed
                 <br />
               </span>
-              <span className="text-neutral-50 text-xl font-normal font-['Open Sans'] leading-7">
+              <span className="text-neutral-50 text-sm md:text-md font-normal font-osans ">
                 Join our email newsletter to stay up to date on the latest news
                 and events!
               </span>
             </div>
-            <div className="">
-              <span className="text-neutral-50 text-sm font-normal font-['Open Sans'] leading-tight">
+            <div className="py-2">
+              <span className="text-neutral-50 text-sm font-normal font-osans leading-tight">
                 Email:
               </span>
               <div className="flex flex-row gap-2 pt-2">
                 <input
                   type="text"
-                  className="w-[297.33px] h-9 p-2.5 bg-neutral-50 rounded-md border border-neutral-50  gap-2.5 inline-flex"
+                  className=" bg-neutral-50 rounded-md border border-neutral-50 inline-flex"
                 />
-                <button className="w-[110px] h-[37px] px-12 bg-rose-200 rounded-[10px] justify-center items-center gap-2.5 flex text-rose-950 text-xl font-semibold font-['Open Sans'] leading-7">
+                <button className=" bg-rose-200 rounded-md justify-center items-center flex text-rose-950 text-sm px-4 font-semibold font-osans ">
                   Submit
                 </button>
               </div>
             </div>
           </div>
         </div>
-        <div className="w-full border-2 border-rose-950 my-10 " />
-        <div className="flex justify-between ">
-          <div className="h-[49px] text-neutral-50 text-sm font-normal font-['Open Sans']">
+        <div className="w-full border-2 border-rose-950 my-4 " />
+        <div className="flex justify-between lg:gap-48">
+          <div className="text-neutral-50 text-sm font-normal font-osans">
             Bottomless Closet is a nonprofit 501(c)(3) organization; EIN/Tax ID
             number 13-4037622 <br />© 2023 Bottomless Closet. All rights
             reserved. Privacy Policy
           </div>
-          <div className="flex items-center gap-6 ">
+          <div className="flex items-center gap-2 md:gap-4 ">
             <Image
-              className=" h-[55px]"
               src="/footer/greatest-nonprofits.png"
               width={75}
               height={55}
               alt=""
+              className="w-10 h-18 md: md:w-auto md:h-auto"
             />
             <Image
-              className="w-[50px] h-[50px]"
               src="/footer/ptrc.png"
               width={50}
               height={50}
               alt=""
+              className="w-10 h-8 md: md:w-auto md:h-auto"
             />
             <Image
-              className="w-[79px] h-[30px]"
               src="/footer/accreditedcharity.png"
               width={79}
               height={30}
               alt=""
+              className="w-10 h-8 md: md:w-auto md:h-auto"
             />
             <Image
-              className="w-[75px] h-[55px]"
               src="/footer/gpge.png"
               width={75}
               height={55}
               alt=""
+              className="w-10 h-8 md: md:w-auto md:h-auto"
             />
           </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,36 @@
-import type { Config } from 'tailwindcss'
-import defaultTheme from 'tailwindcss/defaultTheme';
-import daisyui from "daisyui"
-
-
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+import daisyui from "daisyui";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        mont: ["Montserrat", "sans-serif"],
+        osans: ["Open Sans", "sans-serif"],
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        'core-red': '#B0121A',
-        'dark-red': '#630A0E',
-        'pink-2': '#FFDAD6',
-        'salmon': '#FFB4AC',
-        'dark-green': '#085047',
-        'red-brick': '#8C0E14',
-        'charcoal': '#404040',
-        'light-gray': '#404040',
+        "core-red": "#B0121A",
+        "dark-red": "#630A0E",
+        "pink-2": "#FFDAD6",
+        salmon: "#FFB4AC",
+        "dark-green": "#085047",
+        "red-brick": "#8C0E14",
+        charcoal: "#404040",
+        "light-gray": "#404040",
       },
     },
   },
   plugins: [daisyui],
-}
-export default config
+};
+export default config;

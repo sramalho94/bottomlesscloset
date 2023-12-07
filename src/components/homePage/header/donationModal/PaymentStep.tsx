77dba3731b9a9cd87b13656eaab36ce1b30f3c85
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-12-06 21:52:21 -0500

    payment step

--- src/components/homePage/header/donationModal/PaymentStep.tsx
+++ src/components/homePage/header/donationModal/PaymentStep.tsx
@@ -4,16 +4,28 @@ import { PropsForDonateStep } from '@/types/types'
 
 import SubmitDonationBtn from '../../btns/SubmitDonationBtn';
 
-function PaymentStep({setDonationStep}: PropsForDonateStep) {
+function PaymentStep({ setDonationStep }: PropsForDonateStep) {
+
+    // TODO: get paymentType value from context
+    const [paymentType, setPaymentType] = useState('one-time')
+
     const handleSubmit = () => {
         console.log('Submitted ')
         setDonationStep(4)
     }
-  return (
-    <div>
-        <SubmitDonationBtn handleSubmit={handleSubmit} />
-    </div>
-  )
+    return (
+        <div className="flex flex-col justify-between h-[100%] w-[368px]">
+            <div>
+                <p className='text-[#323232] text-[18px]'>
+                    You are making a
+                    <span className='font-bold'> {paymentType} </span>
+                    donation of
+                </p>
+                <h1 className='font-bold text-black text-[48px]'>$50</h1>
+            </div>
+            <SubmitDonationBtn handleSubmit={handleSubmit} />
+        </div>
+    )
 }
 
 export default PaymentStep
